51163ffbc23fe73a7aa7ef3bfe2ffd1d817cbcd1
Panitsara29 <yunlan.tatitaisakul@gmail.com>
2025-05-14 13:55:52 +0700

    update IMSUK_food

--- IMSUK/last/myproject/myapp/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import CartItem
-
-def cart_item_count(request):
-    if request.user.is_authenticated:
-        total_qty = sum(item.quantity for item in CartItem.objects.filter(user=request.user))
-        return {'cart_item_count': total_qty}
-    return {'cart_item_count': 0}
